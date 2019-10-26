c02de96502f9027f410627134f784e6bd403f49a
Michał Zwierzyński <firewallosiem@gmail.com>
2019-10-26 17:43:17 +0200

    Client - App Configuration

--- Client/App.js
+++ Client/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!}</Text>
-    </View>
-  );
+  Text,
+
+} from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Text>Abcd</Text>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
